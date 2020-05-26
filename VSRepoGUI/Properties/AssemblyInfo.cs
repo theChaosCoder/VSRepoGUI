3d0b2d53438fc667208f481d34fe6ed3e3ce32eb
Unknown <thechaoscoder@gmail.com>
2020-05-27 00:35:10 +0200

    Update nuget packages

--- VSRepoGUI/Properties/AssemblyInfo.cs
+++ VSRepoGUI/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("VSRepoGUI")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
